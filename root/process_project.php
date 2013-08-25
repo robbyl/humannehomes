666d5ac822d64dca58a429b7fae67a0427c74bec
J <J@J-PC>
2013-08-25 14:53:09 +0300

    Merge origin/master
    Conflicts:
    	index.php

--- root/process_project.php
+++ root/process_project.php
@@ -4,7 +4,8 @@ require '../config/config.php';
 require '../functions/general_functions.php';
 
 $projectName = clean($_POST['projectName']);
-$description = clean($_POST['description']);
+$shortdescription = clean($_POST['shortdescription']);
+$fulldescription = clean($_POST['fulldescription']);
 $image_name = clean($_FILES['image']['name']); // Get image name
 //$file_name = clean($_FILES['attachment']['name']); // Get file name
 //
@@ -25,7 +26,7 @@ if (!empty($image_name)) {
         } else {
 
             // Uploading it to image folder.
-            move_uploaded_file($_FILES["image"]["tmp_name"], "uploads/images/" . $image_name);
+            move_uploaded_file($_FILES["image"]["tmp_name"], "../images/portfolio/" . $image_name);
         }
     } else {
 
@@ -36,31 +37,31 @@ if (!empty($image_name)) {
 }
 
 // Get and upload attachment file
-$allowed_file_ext = array("pdf", "doc", "docx");
+//$allowed_file_ext = array("pdf", "doc", "docx");
+//
+//$extension = end(explode(".", $file_name));
+//
+//if (in_array($extension, $allowed_file_ext) && ($_FILES["attachment"]["size"] < 100000000)) {
+//
+//    // Checking file for errors
+//    if ($_FILES["attachment"]["error"] > 0) {
+//
+//        $message = "This file contain errors. Return Code: " . $_FILES["attachment"]["error"];
+//        //info('error', $message);
+//    } else {
+//
+//        // Uploading it to doc folder.
+//        move_uploaded_file($_FILES["attachment"]["tmp_name"], "uploads/docs/" . $file_name);
+//    }
+//} else {
+//
+//    info('error', 'This file type is not allowed');
+//    header('Location: home.php');
+//    exit(0);
+//}
 
-$extension = end(explode(".", $file_name));
-
-if (in_array($extension, $allowed_file_ext) && ($_FILES["attachment"]["size"] < 100000000)) {
-
-    // Checking file for errors
-    if ($_FILES["attachment"]["error"] > 0) {
-
-        $message = "This file contain errors. Return Code: " . $_FILES["attachment"]["error"];
-        //info('error', $message);
-    } else {
-
-        // Uploading it to doc folder.
-        move_uploaded_file($_FILES["attachment"]["tmp_name"], "uploads/docs/" . $file_name);
-    }
-} else {
-
-    info('error', 'This file type is not allowed');
-    header('Location: home.php');
-    exit(0);
-}
-
-$query_news = "INSERT INTO news
-                      (nws_title, nws_posted_date, nws_description, nws_attachment, nws_image)
+$query_news = "INSERT INTO project
+                      (`projectName`, nws_posted_date, nws_description, nws_attachment, nws_image)
                VALUES ('$title', CURRENT_TIMESTAMP(), '$description', '$file_name', '$image_name')";
 
 $result_news = mysqli_query($link, $query_news) or die(mysqli_error());
